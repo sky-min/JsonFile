1b516fb03f07ca4bc21ebbfd5f984ed491a3b326
sky-min <ppt00003333@gmail.com>
2022-09-29 03:29:02 +0000

    ...

--- src/skymin/json/JsonFile.php
+++ src/skymin/json/JsonFile.php
@@ -1,17 +1,4 @@
 <?php
-/*
-*       _                       _        
-*      | |                     (_)       
-*  ___ | | __ _   _  _ __ ___   _  _ __  
-* / __|| |/ /| | | || '_ ` _ \ | || '_ \ 
-* \__ \|   < | |_| || | | | | || || | | |
-* |___/|_|\_\ \__, ||_| |_| |_||_||_| |_|
-*              __/ |                     
-*             |___/                      
-* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
-*/
 
 declare(strict_types = 1);
 
@@ -25,18 +12,19 @@ use function json_decode;
 use const JSON_PRETTY_PRINT;
 use const JSON_UNESCAPED_UNICODE;
 
-final class Data{
+final class JsonFile{
 
-	private function __construct(){
-		//NOOP
+	public array $data
+
+	public function __construct(
+		private string $fileName,
+		array $default = []
+	){
+		$this->data = file_exists($this->fileName) ? json_decode(file_get_contents($this->fileName), true) : $default;
 	}
 
-	public static function call(string $path, array $default = []) : array{
-		return file_exists($path) ? json_decode(file_get_contents($path), true) : $default;
-	}
-	
-	public function save(string $path, array $data) :void{
-		file_put_contents($path . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+	public function save() :void{
+		file_put_contents($this->fileName, json_encode($this->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 	}
 	
 }
